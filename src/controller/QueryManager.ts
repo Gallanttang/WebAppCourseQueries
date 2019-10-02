1f6f07a58ef8d57a17a7fac1ca29447dbb67c86b
a3n1b <a3n1b@ugrad.cs.ubc.ca>
2019-10-02 12:52:37 -0700

    fixed promise handling in isQueryValid and checkFilter

--- src/controller/QueryManager.ts
+++ src/controller/QueryManager.ts
@@ -21,16 +21,12 @@ export default class QueryManager {
                 if (that.equals(Object.keys(query), ["WHERE", "OPTIONS"]) ||
                     that.equals(Object.keys(query), ["OPTIONS", "WHERE"])) {
                     if (query["WHERE"].hasOwnProperty) {
-                        // there's stuff in the "WHERE", NEED TO RECURSE
-                        // if (!this.checkFilter(query["WHERE"], "WHERE", datasets)) {
-                        //     return false;
-                        // }
                         that.checkFilter(query["WHERE"], "WHERE", datasets).then((result: any) => {
                             if (!result) {
                                 return (reject(new InsightError("invalid filter(s)")));
                             }
                         }).catch((err: any) => {
-                            return (reject(new InsightError(err)));
+                            return (err);
                         });
                     }
                     if (query["OPTIONS"].hasOwnProperty) {
@@ -70,42 +66,47 @@ export default class QueryManager {
     }
 
     private checkFilter(filter: any, parent: string, datasets: any): Promise<any> {
+        const that = this;
         return new Promise<any>((resolve, reject) => {
             if (parent === "WHERE") {
                 if (Object.keys(filter).length === 1) {
-                    return this.checkFilter(filter[Object.keys(filter)[0]], Object.keys(filter)[0], datasets);
+                    const childFilter: any = filter[Object.keys(filter)[0]];
+                    that.checkFilter(childFilter, Object.keys(filter)[0], datasets).then((result: any) => {
+                        return result;
+                    }).catch((err: any) => {
+                        return (err);
+                    });
                 } else {
                     return reject(new InsightError("There is more than one filter in WHERE"));
                 }
             } else if (parent === "IS") {
-                const dataset = filter.key[0].split("_", 1)[0];
-                if (!(!datasets.includes(dataset) && Object.keys(filter).length === 1 &&
-                    typeof filter[Object.keys(filter)[0]] === "string")) {
-                    return reject(new InsightError(""));
+                if (that.checkFilterSCOMPParent(filter, datasets) === true) {
+                    return resolve(true);
                 }
+                return reject(false);
             } else if (parent === "EQ" || parent === "GT" || parent === "LT") {
-                if (Object.keys(filter).length === 1) {
-                    let dataset: string = Object.keys(filter)[0].split("_", 1)[0];
-                    if (this.datasetToQuery === "") {
-                        this.datasetToQuery = dataset;
-                    } else if (this.datasetToQuery !== dataset) {
-                        return reject(new InsightError(dataset + " not does not match " + this.datasetToQuery));
-                    }
-                    if (!datasets.includes(dataset) && typeof filter[Object.keys(filter)[0]] === "number") {
-                        return resolve(true);
-                    }
-                } else {
-                    return reject(new InsightError(parent + " does not have only 1 key value pair"));
+                let isFilterValid: any = that.checkFilterMCOMPParent(filter, parent, datasets);
+                if (isFilterValid === true) {
+                    return resolve(true);
                 }
+                return reject(isFilterValid);
             } else if (parent === "NOT" || parent === "OR" || parent === "AND") {
                 if (Object.keys(filter).length >= 1) {
                     for (const key of Object.keys(filter)) {
-                        if (!this.checkFilter(filter[key], key, datasets)) {
-                            return reject(new InsightError(false));
-                        }
+                        that.checkFilter(filter[key], key, datasets).then((result: any) => {
+                            if (!result) {
+                                return reject(new InsightError(false));
+                            }
+                        }).catch((err: any) => {
+                            return (err);
+                        });
                     }
                     let dataset: string = Object.keys(filter)[0].split("_", 1)[0];
-                    return !datasets.includes(dataset) && typeof filter[Object.keys(filter)[0]] === "object";
+                    if (!datasets.includes(dataset) && typeof filter[Object.keys(filter)[0]] === "object") {
+                        return resolve(true);
+                    } else {
+                        return reject(new InsightError(false));
+                    }
                 } else {
                     return reject(new InsightError(false));
                 }
@@ -113,5 +114,33 @@ export default class QueryManager {
                 return reject(new InsightError(false));
             }
         });
+    }
+    /*
+    *  helper called by checkFilter, because checkFilter was getting too long
+    *  should return true if filter is valid, otherwise return statement describing error
+    */
+    private checkFilterMCOMPParent(filter: any, parent: string, datasets: any): any {
+        if (Object.keys(filter).length === 1) {
+            let dataset: string = Object.keys(filter)[0].split("_", 1)[0];
+            if (this.datasetToQuery === "") {
+                this.datasetToQuery = dataset;
+            } else if (this.datasetToQuery !== dataset) {
+                return (dataset + " not does not match " + this.datasetToQuery);
+            }
+            if (!datasets.includes(dataset) && typeof filter[Object.keys(filter)[0]] === "number") {
+                return true;
+            }
+        } else {
+            return (parent + " does not have only 1 key value pair");
+        }
+    }
+    /*
+    *  helper called by checkFilter, because checkFilter was getting too long
+    *  should return true if filter is valid, otherwise return statement describing error
+    */
+    private checkFilterSCOMPParent(filter: any, datasets: any): boolean {
+        const dataset = filter.key[0].split("_", 1)[0];
+        return (!datasets.includes(dataset) && Object.keys(filter).length === 1 &&
+            typeof filter[Object.keys(filter)[0]] === "string");
     }
 }
