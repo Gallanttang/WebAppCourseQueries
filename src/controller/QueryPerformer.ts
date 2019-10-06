e642404312aeeeb0c413b8fdf47cd630a1a9bd13
andrewliu <aliu5454@gmail.com>
2019-10-06 16:23:20 -0700

    Added sort by specific key for array of objects

--- src/controller/QueryPerformer.ts
+++ src/controller/QueryPerformer.ts
@@ -57,8 +57,33 @@ export default class QueryPerformer {
             //       return reject("The result is too big. Only queries with a maximum of 5000 results are supported.");
             //     }
             // }
+            // assume that result is only of the columns chosen
+
+            // sort result given ORDER
+            const orderBy: any = query["ORDER"]; // should give "courses_avg" or something else
+            // sort by given key to order (orderBy) in result
+            result.sort(that.compareValues(orderBy));
             return resolve(result);
         });
+    }
+    public compareValues(key: any): any {
+        return function (a: any, b: any) {
+            if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+                // key does not exist in object
+                return 0;
+            }
+            const varA = (typeof a[key] === "string") ?
+                a[key].toUpperCase() : a[key];
+            const varB = (typeof b[key] === "string") ?
+                b[key].toLowerCase() : b[key];
+            let comparison = 0;
+            if (varA > varB) {
+                comparison = 1;
+            } else if (varA < varB) {
+                comparison = -1;
+            }
+            return comparison;
+        };
     }
 
     public equals(keys: any, expected: any): boolean {
