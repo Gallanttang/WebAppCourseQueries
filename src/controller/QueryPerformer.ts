b4dd10f558d6c3130b2287d6493e0335bbe193b0
andrewliu <aliu5454@gmail.com>
2019-10-06 16:54:53 -0700

    Updated returnQueriedCourses to not be a promise

--- src/controller/QueryPerformer.ts
+++ src/controller/QueryPerformer.ts
@@ -5,6 +5,7 @@ import ASTNode from "./ASTNode";
 
 export default class QueryPerformer {
     private astNode: ASTNode = new ASTNode();
+
     constructor() {
         Log.trace("QueryPerformer::init()");
     }
@@ -15,57 +16,58 @@ export default class QueryPerformer {
      *  A result should have a max size of 5,000. If this is exceeded, promise should reject with a ResultTooLargeError.
      */
     // todo change the test query to ResultTooLargeError!!
-    public returnQueriedCourses(dataStructure: any, query: any): Promise<any[]> {
+    public returnQueriedCourses(dataStructure: any, query: any): any[] {
         let result: any;
         let that = this;
-        return new Promise<any>((resolve) => {
-            const value = query["WHERE"];
-            // query[value] will give ["COLUMNS, "ORDER"]
-            const columns = query["COLUMNS"]; // will give "COLUMNS"
-            const columnsToDisplay = query[columns];
-            if (value.hasOwnProperty) {
-                const firstArgument: string = Object.keys(value)[0];
-                Log.trace(firstArgument);
-                switch (firstArgument) {
-                    case "AND":
-                        result = that.astNode.ANDfunc(dataStructure, value);
-                        break;
-                    case "OR":
-                        result = that.astNode.orFunc(dataStructure, value);
-                        break;
-                    case "NOT":
-                        result = that.astNode.NOTfunc(dataStructure, value);
-                        break;
-                    case "LT":
-                        result = that.astNode.LTfunc(dataStructure, value);
-                        break;
-                    case "EQ":
-                        result = that.astNode.EQfunc(dataStructure, value);
-                        break;
-                    case "GT":
-                        result = that.astNode.GTfunc(dataStructure, value);
-                        break;
-                    case "IS":
-                        result = that.astNode.ISfunc(dataStructure, value);
-                        break;
-                }
-                return result;
-            } else { result = dataStructure; }
-            // else {
-            //     // there's nothing in WHERE. return all results if it's not too large
-            //     if (Object.keys(query["WHERE"]).length === 0) {
-            //       return reject("The result is too big. Only queries with a maximum of 5000 results are supported.");
-            //     }
-            // }
-            // assume that result is only of the columns chosen
+        const value = query["WHERE"];
+        // query[value] will give ["COLUMNS, "ORDER"]
+        const columns = query["COLUMNS"]; // will give "COLUMNS"
+        const columnsToDisplay = query[columns];
+        if (value.hasOwnProperty) {
+            const firstArgument: string = Object.keys(value)[0];
+            Log.trace(firstArgument);
+            switch (firstArgument) {
+                case "AND":
+                    result = that.astNode.ANDfunc(dataStructure, value);
+                    break;
+                case "OR":
+                    result = that.astNode.orFunc(dataStructure, value);
+                    break;
+                case "NOT":
+                    result = that.astNode.NOTfunc(dataStructure, value);
+                    break;
+                case "LT":
+                    result = that.astNode.LTfunc(dataStructure, value);
+                    break;
+                case "EQ":
+                    result = that.astNode.EQfunc(dataStructure, value);
+                    break;
+                case "GT":
+                    result = that.astNode.GTfunc(dataStructure, value);
+                    break;
+                case "IS":
+                    result = that.astNode.ISfunc(dataStructure, value);
+                    break;
+            }
+            return result;
+        } else {
+            result = dataStructure;
+        }
+        // else {
+        //     // there's nothing in WHERE. return all results if it's not too large
+        //     if (Object.keys(query["WHERE"]).length === 0) {
+        //       return reject("The result is too big. Only queries with a maximum of 5000 results are supported.");
+        //     }
+        // }
+        // assume that result is only of the columns chosen
 
-            // sort result given ORDER
-            const orderBy: any = query["ORDER"]; // should give "courses_avg" or something else
-            // sort by given key to order (orderBy) in result
-            result.sort(that.compareValues(orderBy));
-            return resolve(result);
-        });
+        // sort result given ORDER
+        const orderBy: any = query["ORDER"]; // should give "courses_avg" or something else
+        // sort by given key to order (orderBy) in result
+        result.sort(that.compareValues(orderBy));
+        return result;
     }
+
     public compareValues(key: any): any {
         return function (a: any, b: any) {
             if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
