6b76789902535bc270421bc7918d75f356efd0fa
Gallant Tang <gallanttang@gmail.com>
2019-10-06 15:09:33 -0700

    Made major changes to ASTnode

--- src/controller/ASTNode.ts
+++ src/controller/ASTNode.ts
@@ -21,37 +21,35 @@ export default class ASTNode {
     public ANDfunc(dataStructure: any, query: any): any {
         const that = this;
         const childPromises: any = [];
-        return new Promise<string[]>((resolve) => {
-            let results: any = [];
-            // query is going to be in form OR[GT: {courses_avg: 98}, ...]
-            const childQuery = query["AND"]; // this will give [GT: {courses_avg: 98}, ...]
-            if (Array.isArray(childQuery)) {
-                for (const val of childQuery) {
-                    childPromises.push(that.switcher(childQuery, val, dataStructure));
-                }
+        let results: any = [];
+        // query is going to be in form OR[GT: {courses_avg: 98}, ...]
+        const childQuery = query["AND"]; // this will give [GT: {courses_avg: 98}, ...]
+        if (Array.isArray(childQuery)) {
+            for (const val of childQuery) {
+                childPromises.push(that.switcher(childQuery, val, dataStructure));
             }
-            for (let childResults of childPromises) {
-                let tempResult = childResults[0]as Record<string, any>;
-                for (const cr of childResults) {
-                    let restrictedCR = cr as Record<string, any>;
-                    if (childResults.indexOf(cr) > 0 ) {
-                        let deptKeys = Object.keys(cr);
-                        for (const key of deptKeys) {
-                            if (tempResult.hasOwnProperty(key)) {
-                                // only keep elements which are also found in the cr
-                                tempResult[key] = tempResult[key].filter((f: any) => restrictedCR[key].includes(f));
-                                // if any key becomes empty of all elements, delete it
-                                if (!tempResult[key].hasOwnProperty) {
-                                    tempResult.splice(tempResult[key].indexOf, 1);
-                                }
+        }
+        for (let childResults of childPromises) {
+            let tempResult = childResults[0] as Record<string, any>;
+            for (const cr of childResults) {
+                let restrictedCR = cr as Record<string, any>;
+                if (childResults.indexOf(cr) > 0) {
+                    let deptKeys = Object.keys(cr);
+                    for (const key of deptKeys) {
+                        if (tempResult.hasOwnProperty(key)) {
+                            // only keep elements which are also found in the cr
+                            tempResult[key] = tempResult[key].filter((f: any) => restrictedCR[key].includes(f));
+                            // if any key becomes empty of all elements, delete it
+                            if (!tempResult[key].hasOwnProperty) {
+                                tempResult.splice(tempResult[key].indexOf, 1);
                             }
                         }
                     }
                 }
-                results = tempResult;
             }
-            return results;
-        });
+            results = tempResult;
+        }
+        return results;
     }
     /**
      * @param dataStructure
@@ -61,37 +59,35 @@ export default class ASTNode {
     public orFunc(dataStructure: any, query: any): any {
         const that = this;
         const childPromises: any = [];
-        return new Promise<string[]>((resolve) => {
-            let result: any = [];
-            // query is going to be in form AND[GT: {courses_avg: 98}, ...]
-            const childQuery = query["OR"]; // this will give [GT: {courses_avg: 98}, ...]
-            if (Array.isArray(childQuery)) {
-                for (const val of childQuery) {
-                    childPromises.push(this.switcher(childQuery, val, dataStructure));
-                }
+        let result: any = [];
+        // query is going to be in form AND[GT: {courses_avg: 98}, ...]
+        const childQuery = query["OR"]; // this will give [GT: {courses_avg: 98}, ...]
+        if (Array.isArray(childQuery)) {
+            for (const val of childQuery) {
+                childPromises.push(this.switcher(childQuery, val, dataStructure));
             }
-            for (let childResults of childPromises) {
-                let tempResult = childResults[0]as Record<string, any>;
-                for (const cr of childResults) {
-                    let restrictedCR = cr as Record<string, any>;
-                    let index = childResults.indexOf(cr);
-                    if (index > 0 ) {
-                        let deptKeys = Object.keys(cr);
-                        for (const key of deptKeys) {
-                            if (tempResult.hasOwnProperty(key)) {
-                                // concatenate
-                                // not sure if this way of using as Record<string, any> is ok ?
-                                tempResult[key].concat(restrictedCR[key]);
-                            } else {
-                                tempResult.push({ [key] : restrictedCR[key]});
-                            }
+        }
+        for (let childResults of childPromises) {
+            let tempResult = childResults[0] as Record<string, any>;
+            for (const cr of childResults) {
+                let restrictedCR = cr as Record<string, any>;
+                let index = childResults.indexOf(cr);
+                if (index > 0) {
+                    let deptKeys = Object.keys(cr);
+                    for (const key of deptKeys) {
+                        if (tempResult.hasOwnProperty(key)) {
+                            // concatenate
+                            // not sure if this way of using as Record<string, any> is ok ?
+                            tempResult[key].concat(restrictedCR[key]);
+                        } else {
+                            tempResult.push({[key]: restrictedCR[key]});
                         }
                     }
                 }
-                result = tempResult;
             }
-            return resolve(result);
-        });
+            result = tempResult;
+        }
+        return result;
     }
     public NOTfunc(dataStructure: any, query: any): any {
         let that = this;
