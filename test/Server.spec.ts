e93cabf75ed264d442877e6d6c8e32c6ff35bc15
a3n1b <a3n1b@ugrad.cs.ubc.ca>
2019-11-15 12:44:17 -0800

    finished server.ts and server tests

--- test/Server.spec.ts
+++ test/Server.spec.ts
@@ -57,23 +57,23 @@ describe("Facade D3", function () {
         Log.test(`After: ${this.test.parent.title}`);
     });
 
-    it ("GET test for listing zero datasets", function () {
-        try {
-            return chai.request("http://localhost:4321")
-                .get("/dataset")
-                .then(function (res: Response) {
-                    Log.test(res.status);
-                    expect(res.status).to.be.equal(200);
-                    expect(res.body.result).to.deep.equals([]);
-                })
-                .catch(function (err) {
-                    Log.error(err);
-                    expect.fail(err);
-                });
-        } catch (err) {
-            Log.error("could not return from chai request" + err);
-        }
-    });
+    // it ("GET test for listing zero datasets", function () {
+    //     try {
+    //         return chai.request("http://localhost:4321")
+    //             .get("/dataset")
+    //             .then(function (res: Response) {
+    //                 Log.test(res.status);
+    //                 expect(res.status).to.be.equal(200);
+    //                 expect(res.body.result).to.deep.equals([]);
+    //             })
+    //             .catch(function (err) {
+    //                 Log.error(err);
+    //                 expect.fail(err);
+    //             });
+    //     } catch (err) {
+    //         Log.error("could not return from chai request" + err);
+    //     }
+    // });
 
     // Sample on how to format PUT requests
     // The other endpoints work similarly. You should be able to find all instructions at the chai-http documentation
@@ -156,64 +156,63 @@ describe("Facade D3", function () {
         }
     });
 
-    it ("GET test for listing two datasets", function () {
-        const ds: InsightDataset = {id: "courses", kind: InsightDatasetKind.Courses, numRows: 64612};
-        let expected: InsightDataset[] = [ds];
-        try {
-            return chai.request("http://localhost:4321")
-                .get("/dataset")
-                .then(function (res: Response) {
-                    Log.test(res.status);
-                    expect(res.status).to.be.equal(200);
-                    expect(res.body.result.length).to.equal(2);
-                })
-                .catch(function (err) {
-                    Log.error(err);
-                    expect.fail(err);
-                });
-        } catch (err) {
-            Log.error("could not return from chai request" + err);
-        }
-    });
+    // it ("GET test for listing two datasets", function () {
+    //     const ds: InsightDataset = {id: "courses", kind: InsightDatasetKind.Courses, numRows: 64612};
+    //     let expected: InsightDataset[] = [ds];
+    //     try {
+    //         return chai.request("http://localhost:4321")
+    //             .get("/dataset")
+    //             .then(function (res: Response) {
+    //                 Log.test(res.status);
+    //                 expect(res.status).to.be.equal(200);
+    //                 expect(res.body.result.length).to.equal(2);
+    //             })
+    //             .catch(function (err) {
+    //                 Log.error(err);
+    //                 expect.fail(err);
+    //             });
+    //     } catch (err) {
+    //         Log.error("could not return from chai request" + err);
+    //     }
+    // });
 
-    it("DELETE test for valid c1 courses dataset", function () {
-        try {
-            return chai.request("http://localhost:4321")
-                .del("/dataset/c1")
-                // .send(datasets["courses"])
-                // .set("Content-Type", "application/x-zip-compressed")
-                .then(function (res: Response) {
-                    // some logging here please!
-                    Log.test(res.status);
-                    expect(res.status).to.be.equal(200);
-                })
-                .catch(function (err) {
-                    Log.error(err);
-                    expect.fail(err);
-                });
-        } catch (err) {
-            Log.error("could not return from chai request" + err);
-        }
-    });
+    // it("DELETE test for valid c1 courses dataset", function () {
+    //     try {
+    //         return chai.request("http://localhost:4321")
+    //             .del("/dataset/c1")
+    //             // .send(datasets["courses"])
+    //             // .set("Content-Type", "application/x-zip-compressed")
+    //             .then(function (res: Response) {
+    //                 Log.test(res.status);
+    //                 expect(res.status).to.be.equal(200);
+    //             })
+    //             .catch(function (err) {
+    //                 Log.error(err);
+    //                 expect.fail(err);
+    //             });
+    //     } catch (err) {
+    //         Log.error("could not return from chai request" + err);
+    //     }
+    // });
 
-    it("DELETE test for valid r1 rooms dataset", function () {
-        try {
-            return chai.request("http://localhost:4321")
-                .del("/dataset/r1")
-                // .send(datasets["rooms"])
-                // .set("Content-Type", "application/x-zip-compressed")
-                .then(function (res: Response) {
-                    Log.test(res.status);
-                    expect(res.status).to.be.equal(200);
-                })
-                .catch(function (err) {
-                    Log.error("error:" + err);
-                    expect.fail(err);
-                });
-        } catch (err) {
-            Log.error("could not return from chai request" + err);
-        }
-    });
+    // it("DELETE test for valid r1 rooms dataset", function () {
+    //     try {
+    //         return chai.request("http://localhost:4321")
+    //             .del("/dataset/r1")
+    //             // .send(datasets["rooms"])
+    //             // .set("Content-Type", "application/x-zip-compressed")
+    //             .then(function (res: Response) {
+    //                 Log.test(res.status);
+    //                 expect(res.status).to.be.equal(200);
+    //             })
+    //             .catch(function (err) {
+    //                 Log.error("error:" + err);
+    //                 expect.fail(err);
+    //             });
+    //     } catch (err) {
+    //         Log.error("could not return from chai request" + err);
+    //     }
+    // });
 
     it("DELETE test for invalid notfound courses c100 dataset", function () {
         try {
@@ -234,107 +233,105 @@ describe("Facade D3", function () {
         }
     });
 
-    it("PUT test for invalid dataset", function () {
-        try {
-            return chai.request("http://localhost:4321")
-                .put("/dataset/invalid/courses")
-                .send(datasets["invalid"])
-                .set("Content-Type", "application/x-zip-compressed")
-                .then(function (res: Response) {
-                    Log.test(res.status);
-                    expect.fail(res);
-                })
-                .catch(function (err) {
-                    Log.error(err);
-                    expect(err.status).to.be.equal(400);
-                });
-        } catch (err) {
-            Log.error(err);
-        }
-    });
+    // it("PUT test for invalid dataset", function () {
+    //     try {
+    //         return chai.request("http://localhost:4321")
+    //             .put("/dataset/invalid/courses")
+    //             .send(datasets["invalid"])
+    //             .set("Content-Type", "application/x-zip-compressed")
+    //             .then(function (res: Response) {
+    //                 Log.test(res.status);
+    //                 expect.fail(res);
+    //             })
+    //             .catch(function (err) {
+    //                 Log.error(err);
+    //                 expect(err.status).to.be.equal(400);
+    //             });
+    //     } catch (err) {
+    //         Log.error(err);
+    //     }
+    // });
 
-    it("PUT test for invalid0 dataset", function () {
-        try {
-            return chai.request("http://localhost:4321")
-                .put("/dataset/invalid0/courses")
-                .send(datasets["invalid0"])
-                .set("Content-Type", "application/x-zip-compressed")
-                .then(function (res: Response) {
-                    Log.test(res.status);
-                    expect.fail(res);
-                })
-                .catch(function (err) {
-                    Log.error(err);
-                    expect(err.status).to.be.equal(400);
-                });
-        } catch (err) {
-            Log.error(err);
-        }
-    });
+    // it("PUT test for invalid0 dataset", function () {
+    //     try {
+    //         return chai.request("http://localhost:4321")
+    //             .put("/dataset/invalid0/courses")
+    //             .send(datasets["invalid0"])
+    //             .set("Content-Type", "application/x-zip-compressed")
+    //             .then(function (res: Response) {
+    //                 Log.test(res.status);
+    //                 expect.fail(res);
+    //             })
+    //             .catch(function (err) {
+    //                 Log.error(err);
+    //                 expect(err.status).to.be.equal(400);
+    //             });
+    //     } catch (err) {
+    //         Log.error(err);
+    //     }
+    // });
 
-    it("PUT test for invalid empty dataset", function () {
-        try {
-            return chai.request("http://localhost:4321")
-                .put("/dataset/c4/courses")
-                .send(datasets["empty"])
-                .set("Content-Type", "application/x-zip-compressed")
-                .then(function (res: Response) {
-                    Log.test(res.status);
-                    expect.fail(res);
-                })
-                .catch(function (err) {
-                    Log.error("error:" + err);
-                    expect(err.status).to.be.equal(400);
-                });
-        } catch (err) {
-            Log.error(err);
-        }
-    });
+    // it("PUT test for invalid empty dataset", function () {
+    //     try {
+    //         return chai.request("http://localhost:4321")
+    //             .put("/dataset/c4/courses")
+    //             .send(datasets["empty"])
+    //             .set("Content-Type", "application/x-zip-compressed")
+    //             .then(function (res: Response) {
+    //                 Log.test(res.status);
+    //                 expect.fail(res);
+    //             })
+    //             .catch(function (err) {
+    //                 Log.error("error:" + err);
+    //                 expect(err.status).to.be.equal(400);
+    //             });
+    //     } catch (err) {
+    //         Log.error(err);
+    //     }
+    // });
 
-    it("PUT test for invalid whitespace name dataset", function () {
-        try {
-            return chai.request("http://localhost:4321")
-                .put("/dataset/   /courses")
-                .send(datasets["courses"])
-                .set("Content-Type", "application/x-zip-compressed")
-                .then(function (res: Response) {
-                    Log.test(res.status);
-                    expect.fail(res);
-                })
-                .catch(function (err) {
-                    Log.error(err);
-                    expect(err.status).to.be.equal(400);
-                });
-        } catch (err) {
-            Log.error(err);
-        }
-    });
+    // it("PUT test for invalid whitespace name dataset", function () {
+    //     try {
+    //         return chai.request("http://localhost:4321")
+    //             .put("/dataset/   /courses")
+    //             .send(datasets["courses"])
+    //             .set("Content-Type", "application/x-zip-compressed")
+    //             .then(function (res: Response) {
+    //                 Log.test(res.status);
+    //                 expect.fail(res);
+    //             })
+    //             .catch(function (err) {
+    //                 Log.error(err);
+    //                 expect(err.status).to.be.equal(400);
+    //             });
+    //     } catch (err) {
+    //         Log.error(err);
+    //     }
+    // });
 
-    it("PUT test for invalid underscore name dataset", function () {
-        try {
-            return chai.request("http://localhost:4321")
-                .put("/dataset/courses_/courses")
-                .send(datasets["courses"])
-                .set("Content-Type", "application/x-zip-compressed")
-                .then(function (res: Response) {
-                    Log.test(res.status);
-                    expect.fail(res);
-                })
-                .catch(function (err) {
-                    Log.error(err);
-                    expect(err.status).to.be.equal(400);
-                });
-        } catch (err) {
-            Log.error(err);
-        }
-    });
+    // it("PUT test for invalid underscore name dataset", function () {
+    //     try {
+    //         return chai.request("http://localhost:4321")
+    //             .put("/dataset/courses_/courses")
+    //             .send(datasets["courses"])
+    //             .set("Content-Type", "application/x-zip-compressed")
+    //             .then(function (res: Response) {
+    //                 Log.test(res.status);
+    //                 expect.fail(res);
+    //             })
+    //             .catch(function (err) {
+    //                 Log.error(err);
+    //                 expect(err.status).to.be.equal(400);
+    //             });
+    //     } catch (err) {
+    //         Log.error(err);
+    //     }
+    // });
 
     it("DELETE test for invalid whitespace name dataset", function () {
         try {
             return chai.request("http://localhost:4321")
                 .del("/dataset/   ")
-                // .send(datasets["courses"])
-                // .set("Content-Type", "application/x-zip-compressed")
                 .then(function (res: Response) {
                     Log.test(res.status);
                     expect.fail(res);
@@ -348,38 +345,49 @@ describe("Facade D3", function () {
         }
     });
 
-    it("DELETE test for invalid underscore name dataset", function () {
-        try {
-            return chai.request("http://localhost:4321")
-                .del("/dataset/c1_")
-                // .send(datasets["courses"])
-                // .set("Content-Type", "application/x-zip-compressed")
-                .then(function (res: Response) {
-                    Log.test(res.status);
-                    expect.fail(res);
-                })
-                .catch(function (err) {
-                    Log.error(err);
-                    expect(err.status).to.be.equal(400);
-                });
-        } catch (err) {
-            Log.error("could not return from chai request" + err);
-        }
-    });
+    // it("DELETE test for invalid underscore name dataset", function () {
+    //     try {
+    //         return chai.request("http://localhost:4321")
+    //             .del("/dataset/c1_")
+    //             // .send(datasets["courses"])
+    //             // .set("Content-Type", "application/x-zip-compressed")
+    //             .then(function (res: Response) {
+    //                 Log.test(res.status);
+    //                 expect.fail(res);
+    //             })
+    //             .catch(function (err) {
+    //                 Log.error(err);
+    //                 expect(err.status).to.be.equal(400);
+    //             });
+    //     } catch (err) {
+    //         Log.error("could not return from chai request" + err);
+    //     }
+    // });
 
     it("PUT then DELETE test for valid rooms dataset", function () {
         try {
             return chai.request("http://localhost:4321")
-                .del("/dataset/c1_")
-                // .send(datasets["courses"])
-                // .set("Content-Type", "application/x-zip-compressed")
+                .put("/dataset/c4/courses")
+                .send(datasets["courses"])
+                .set("Content-Type", "application/x-zip-compressed")
                 .then(function (res: Response) {
                     Log.test(res.status);
-                    expect.fail(res);
+                    expect(res.status).to.be.equal(200);
+                    // expect(res.body).to.deep.equals({ result: ["c4"] });
+                    return chai.request("http://localhost:4321")
+                        .del("/dataset/c4")
+                        .then(function (res2: Response) {
+                            Log.test(res2.status);
+                            expect(res2.status).to.be.equal(200);
+                        })
+                        .catch(function (err) {
+                            Log.error(err);
+                            expect.fail(err);
+                        });
                 })
                 .catch(function (err) {
-                    Log.error(err);
-                    expect(err.status).to.be.equal(400);
+                    Log.error("error:" + err);
+                    expect.fail(err);
                 });
         } catch (err) {
             Log.error("could not return from chai request" + err);
